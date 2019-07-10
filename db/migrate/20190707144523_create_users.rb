b90970914721751757d65f622467618dce83f876
yuta <yuta.s1182@gmail.com>
2019-07-10 23:01:50 +0900

    [add]home画面追加
    home画面追加のためhomeコントローラやviewを追加

--- db/migrate/20190707144523_create_users.rb
+++ db/migrate/20190707144523_create_users.rb
@@ -1,7 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    drop_table :users
+  end
+  def down
     create_table :users do |t|
-
+      t.string :name
+      t.string :email
+      t.string :address
+      t.string :tel
+      t.string :password_digest
+      t.boolean defalt: false
       t.timestamps
     end
   end
